30144a539b7e4fde14b384e04b126f23475bb525
Come Raczy <come.raczy@gmail.com>
2024-06-11 17:13:02 -0700

    add list of diagnostics with LSP of origin

--- nvim.kickstart/.config/nvim/lua/keymaps.lua
+++ nvim.kickstart/.config/nvim/lua/keymaps.lua
@@ -10,6 +10,12 @@ vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagn
 vim.keymap.set("n", "<leader>de", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
 vim.keymap.set("n", "<leader>dq", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
+vim.keymap.set(
+  "n",
+  "<leader>dl",
+  require("diagnostics").line_diagnostics,
+  { desc = "Show diagnostic [L]SP end error messages" }
+)
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
