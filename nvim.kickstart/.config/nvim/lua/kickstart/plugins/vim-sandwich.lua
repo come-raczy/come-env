023d7320a17d6f059bb5c7f2a113660860873e7a
Come Raczy <come.raczy@gmail.com>
2024-07-09 09:55:05 -0700

    closes #158

---
+++ nvim.kickstart/.config/nvim/lua/kickstart/plugins/vim-sandwich.lua
@@ -0,0 +1,11 @@
+return {
+  "machakann/vim-sandwich",
+  keys = { "sa", "sd", "sr", "sdb", "srb" },
+  -- version = "*", -- Use for stability; omit to use `main` branch for the latest features
+  -- event = "VeryLazy",
+  -- config = function()
+  --   require("vim-sandwich").setup({
+  --     -- Configuration here, or leave empty to use defaults
+  --   })
+  -- end,
+}
